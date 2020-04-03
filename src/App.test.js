9123c7cf98b4c1f692cbdf255de3b6f1bde7e9ab
Rodia <lunaticfriki@gmail.com>
2020-04-03 20:26:03 +0200

    Update App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import renderer from 'react-test-renderer'
 import { GnomeContext } from './context/gnomeContext'
 import App from './App'
