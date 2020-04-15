656ccbd44bb5b2f6f143ae0c0f09e9900fd0ea2d
lunaticfriki <48156929+lunaticfriki@users.noreply.github.com>
2020-04-15 15:13:04 +0200

    Cypress fix

--- cypress/integration/about.spec.js
+++ cypress/integration/about.spec.js
@@ -1,6 +1,6 @@
 describe('About info suite', () => {
   context('Go to about', () => {
-    it('Should go to aout page', () => {
+    it('Should go to about page', () => {
       cy.visitHome()
       cy.wait(2000)
       cy.get('li').eq(2).click()
